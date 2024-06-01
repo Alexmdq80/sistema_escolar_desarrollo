e1887d34bc123e70e05f228d58bb2121648dc3b6
Alex <alexmdq80@gmail.com>
2024-06-01 15:21:11 -0300

    Armando rutas. Registro funcionando.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Auth\LogoutController;
+use App\Http\Controllers\Api\V1\Auth\RegistroController;
+use App\Http\Controllers\Api\V1\Auth\PerfilController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('perfil', [PerfilController::class, 'show']);
+    Route::put('perfil', [PerfilController::class, 'update']);
+    Route::post('auth/logout', LogoutController::class);
+
+    // Route::post('alquileres/inicio', [AlquilerController::class, 'inicio']);
+    // Route::put('alquileres/finalizar/{alquiler}', [AlquilerController::class, 'finalizar']);
+    // Route::get('alquileres', [AlquilerController::class, 'index']);
+
+    // // Rutas que no requieren el middleware "es_admin"
+    // Route::apiResource('bicicletas', BicicletaController::class)->only(['index', 'show']);
+
+    // // Rutas que requieren el middleware "es_admin"
+    // Route::apiResource('bicicletas', BicicletaController::class)->except(['index', 'show'])->middleware('es_admin');
 });
+
+
+Route::post('auth/registro', RegistroController::class)->name('registro');
+Route::post('auth/login', LoginController::class)->name('login');
--- routes/web.php
+++ routes/web.php
@@ -27,63 +27,64 @@ use App\Models\Estudiante_Adulto_Vinculo;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('prueba', function() {
+
+//     // $escuela = Escuela::find(1);
+
+//     // return $escuela->nivel;
+
+//     // DB::enableQueryLog();
+
+//     // $nivel = Nivel::find(1);
+    
+//     // $escuelas = $nivel->escuela;
+
+//     // dd(DB::getQueryLog());
+
+//     // return $escuelas;
+
+//     // $escuela = Escuela::find(10109);
+
+//     // return [$escuela->localidad_asentamiento,
+//     //         $escuela->departamento,
+//     //         $escuela->provincia,
+//     //         $escuela->pais,
+//     //         $escuela->continente,
+//     //         $escuela->ambito, 
+//     //         $escuela->dependencia,
+//     //         $escuela->sector];
+//     // $persona = Persona::find(3);
+//     // return [
+//     //         $persona,
+//     //         $persona->documento_tipo,
+//     //         $persona->documento_situacion,
+//     //         $persona->sexo,
+//     //         $persona->genero,
+//     //         $persona->nacionalidad,
+//     //         $persona->nacimiento_pais,
+//     //         $persona->nacimiento_provincia,
+//     //         $persona->nacimiento_departamento,
+//     //         $persona->nacimiento_localidad_asentamiento
+//     // ];
+//     // $calle = Calle::find(246);
+//     // return $calle->continente;
+
+//     // $vinculo = Responsable_Vinculo::find(2);
+//     // return $vinculo->personas_responsables;
+//     // $persona = Persona::find(1);
+//     // return [$persona, $persona->responsables, $persona->estudiante_vinculos];
+//     // $domicilio = Domicilio::find(1);
+//     // return $domicilio->localidad_asentamiento;
+//     // $contacto = Contacto::find(1);
+//     // return $contacto->persona;
+//     // $inscripcion = Inscripcion::find(1);
+//     // return $inscripcion->escuela_destino;
+//     $r = Estudiante_Adulto_Vinculo::where('id_adulto_vinculo',9)->get();
+    
+//     return $r;
 // });
 
-Route::get('prueba', function() {
-
-    // $escuela = Escuela::find(1);
-
-    // return $escuela->nivel;
-
-    // DB::enableQueryLog();
-
-    // $nivel = Nivel::find(1);
-    
-    // $escuelas = $nivel->escuela;
-
-    // dd(DB::getQueryLog());
-
-    // return $escuelas;
-
-    // $escuela = Escuela::find(10109);
-
-    // return [$escuela->localidad_asentamiento,
-    //         $escuela->departamento,
-    //         $escuela->provincia,
-    //         $escuela->pais,
-    //         $escuela->continente,
-    //         $escuela->ambito, 
-    //         $escuela->dependencia,
-    //         $escuela->sector];
-    // $persona = Persona::find(3);
-    // return [
-    //         $persona,
-    //         $persona->documento_tipo,
-    //         $persona->documento_situacion,
-    //         $persona->sexo,
-    //         $persona->genero,
-    //         $persona->nacionalidad,
-    //         $persona->nacimiento_pais,
-    //         $persona->nacimiento_provincia,
-    //         $persona->nacimiento_departamento,
-    //         $persona->nacimiento_localidad_asentamiento
-    // ];
-    // $calle = Calle::find(246);
-    // return $calle->continente;
-
-    // $vinculo = Responsable_Vinculo::find(2);
-    // return $vinculo->personas_responsables;
-    // $persona = Persona::find(1);
-    // return [$persona, $persona->responsables, $persona->estudiante_vinculos];
-    // $domicilio = Domicilio::find(1);
-    // return $domicilio->localidad_asentamiento;
-    // $contacto = Contacto::find(1);
-    // return $contacto->persona;
-    // $inscripcion = Inscripcion::find(1);
-    // return $inscripcion->escuela_destino;
-    $r = Estudiante_Adulto_Vinculo::where('id_adulto_vinculo',9)->get();
-    
-    return $r;
-});
