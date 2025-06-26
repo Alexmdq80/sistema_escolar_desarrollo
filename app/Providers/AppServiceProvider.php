07403bb26a82613df73652b02978a2ce16f793ca
Alex <alexmdq80@gmail.com>
2025-06-26 11:19:05 -0300

    Agregué requisitos a la password Defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password; // ¡Importa esta clase!
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+    // Define tus reglas de contraseña por defecto aquí
+        Password::defaults(function () {
+            $rule = Password::min(8)
+                ->mixedCase()  // Requiere mayúsculas y minúsculas
+                ->letters()    // Requiere al menos una letra
+                ->numbers()    // Requiere al menos un número
+                ->symbols();   // Requiere al menos un símbolo
+
+            // Opcional: Solo si estás en producción, puedes añadir uncompromised()
+            // para chequear si la contraseña ha sido comprometida en filtraciones.
+            //if ($this->app->isProduction()) {
+            //    $rule->uncompromised();
+            //}
+
+            return $rule;
+        });
     }
 }
